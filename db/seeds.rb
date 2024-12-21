acac78bd9898614b1c67c66057e02f98927363ca
rayz arain <79001879+authorminator@users.noreply.github.com>
2024-12-21 17:48:20 +0900

    Rails Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+10.times do
+  Article.create(title: Faker::Movies::HarryPotter.character, content: Faker::Movies::HarryPotter.quote)
+end
+
+# 10.times do
+#   puts Faker::Movies::HarryPotter.character
+#   puts Faker::Movies::HarryPotter.quote
+# end
